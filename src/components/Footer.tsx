50f4b38c51e02457df6c40d15bee620997f0cc72
Bernardo Antunes <bernasss12@gmail.com>
2024-03-11 11:15:38 +0000

    Change icon dependency because font awesome is very lackluster.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,5 @@
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faGithub, faLinkedin} from "@fortawesome/free-brands-svg-icons";
 import React from "react";
+import { SiGithub, SiLinkedin } from "@icons-pack/react-simple-icons"
 
 function Footer() {
     return (
@@ -10,12 +9,12 @@ function Footer() {
                     <div className="col">
                         <a href="https://github.com/Bernasss12" className="text-decoration-none" target="_blank"
                            rel="noopener noreferrer">
-                            <FontAwesomeIcon icon={faGithub} size='2x' className='icon-color'/>
+                            <SiGithub size={32} className='icon-color'/>
                         </a>
-                        <a href="https://www.linkedin.com/in/bernardo-antunes-9151b818b/" className="text-decoration-none ms-3"
+                        <a href="https://www.linkedin.com/in/bernardo-antunes-9151b818b/"
+                           className="text-decoration-none ms-3"
                            target="_blank" rel="noopener noreferrer">
-                            <FontAwesomeIcon icon={faLinkedin} size='2x' className='icon-color'/>
-                        </a>
+                            <SiLinkedin size={32} className='icon-color'/>                        </a>
                     </div>
                     <div className="col text-end">
                         <small className="text-muted">© 2024 Bernardo Antunes</small>
